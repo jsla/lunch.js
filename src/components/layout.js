9b3db92e4ef61bf0f15e780f4876a6153a05bed7
Angus Perkerson <angusp@angusp.com>
2019-12-02 17:46:45 -0800

    feat: adds gatsby starter project (#13)
    closes: #4
    
    * chore: archives old site
    
    Places old site files in an archive folder
    
    * feat: adds gatsby starter project
    
    Adds gatsby starter project with styled-components
    and Grommet UI-Library
    
    * fix(license): updates license copy
    
    * fix: reverts archiving the documetation files
    
    Reverts the documentation files from being archived
    
    * fix: removes unused components and code
    
    * fix: removes redundant archived files
    
    * feat: updates the package.json fields
    
    * fix: removes unused images

---
+++ src/components/layout.js
@@ -0,0 +1,86 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+import React from "react";
+import PropTypes from "prop-types";
+import { useStaticQuery, graphql } from "gatsby";
+import { Grommet, Box, Grid } from "grommet";
+import styled, { createGlobalStyle } from "styled-components";
+import { Normalize } from "styled-normalize";
+import theme from "../styles/theme";
+
+const GlobalStyle = createGlobalStyle`
+  html,
+  body {
+    height: 100%;
+    font-family: 'Open Sans', sans-serif;
+  }
+`;
+
+const CustomGrommet = styled(Grommet)`
+  background: linear-gradient(
+    60deg,
+    rgba(255, 165, 150, 0.5) 5%,
+    rgba(255, 228, 0, 0.35)
+  );
+  font-family: inherit;
+`;
+
+const AppGrid = styled(Grid)`
+  min-height: 100vh;
+`;
+
+const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+
+  return (
+    <>
+      <Normalize />
+      <GlobalStyle />
+      <CustomGrommet theme={theme}>
+        <AppGrid
+          fill
+          rows={["auto", "flex", "auto"]}
+          columns={["flex"]}
+          areas={[
+            { name: "header", start: [0, 0], end: [1, 0] },
+            { name: "main", start: [0, 1], end: [1, 1] },
+            { name: "footer", start: [0, 2], end: [1, 2] }
+          ]}
+        >
+          <Box gridArea="main" justify="center" align="center">
+            {children}
+          </Box>
+          <Box
+            gridArea="footer"
+            justify="center"
+            align="center"
+            pad={{ horizontal: "large", vertical: "small" }}
+          >
+            © {new Date().getFullYear()}, Built with
+            {` `}
+            <a href="https://www.gatsbyjs.org">Gatsby</a>
+          </Box>
+        </AppGrid>
+      </CustomGrommet>
+    </>
+  );
+};
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired
+};
+
+export default Layout;
